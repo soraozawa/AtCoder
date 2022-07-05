b77d6369dd301a02c09a2dc2b1ac551c7d64a4f1
Sora Ozawa <soraozawa56@gmail.com>
2022-07-06 08:49:56 +0900

    AC

--- 258/D.cpp
+++ 258/D.cpp
@@ -1,29 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 int main(void)
 {
     int n, x;
     cin >> n >> x;
-    int a[n], b[n];
-    for(int i=0;i<n;i++){
+    // int a[n], b[n];
+    vector<int> a(n);
+    vector<int> b(n);
+    for(int i=0;i<n;++i){
         cin >> a[i] >> b[i];
     }
-    int min_minutes=1000000000;
-    for(int M=0;M<n;M++){
-        int sum=0;
-        int min_times=100000000;
-        if(M <= x){
-            for(int i=0;i<M;i++){
-                sum += a[i];
-                sum += b[i];
-                min_times = min(min_times, b[i]);
-            }
-            sum += min_times * (x - M);
-            min_minutes = min(min_minutes, sum);
-            cout << min_times << endl;
+    ll ans = numeric_limits<ll>:: max(), sum = 0;
+    for(int i=0;i<n;++i){
+        if(i+1 > x){
+            break;
         }
+        sum += a[i] + b[i];
+        ans = min(ans, sum + (ll)b[i] * (x - i - 1));
     }
-    cout << min_minutes << endl;
+    // int min_minutes=1000000000;
+    // for(int M=0;M<n;M++){
+    //     int sum=0;
+    //     int min_times=100000000;
+    //     if(M <= x){
+    //         for(int i=0;i<M;i++){
+    //             sum += a[i];
+    //             sum += b[i];
+    //             min_times = min(min_times, b[i]);
+    //         }
+    //         sum += min_times * (x - M);
+    //         min_minutes = min(min_minutes, sum);
+    //         cout << min_times << endl;
+    //     }
+    // }
+    cout << ans << endl;
     return 0;
 }
