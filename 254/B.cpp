2aa31fc08d6f0b2d291445e33ffb87248d68434c
soraozawa <soraozawa56@gmail.com>
2022-07-11 13:14:07 +0900

    AC

--- 254/B.cpp
+++ 254/B.cpp
@@ -8,15 +8,15 @@ int main(void)
     int n;
     cin >> n;
     vector<vector<int>> a(n);
+    rep(i, n)a[i] = vector<int>(i+1);
+
     for(int i=0;i<n;i++){
-        for(int j=0;j<n+1;j++){
+        for(int j=0;j<i+1;j++){
             int ret;
-            if(j==0 || i==j) ret = 1, cout << 1;
+            if(j==0 || i==j) ret = 1;
             else ret = a[i-1][j-1] + a[i-1][j];
             cout << ret << ' ';
-            cout << 1;
             a[i][j] = ret;
-            cout << 1;
         }
         cout << endl;
     }
