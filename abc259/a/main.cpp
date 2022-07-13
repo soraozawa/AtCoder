e02061c3cb98bfb69af598641ac5fac6c626bce0
soraozawa <soraozawa56@gmail.com>
2022-07-13 14:52:51 +0900

    wip

---
+++ abc259/a/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, ans=0;
+    cin >> N;
+    ll sx, sy, tx, ty;
+
+    // 無向片を貼る
+    // スタート地点、ゴール地点を判定
+    // 辺で繋がっているかどうかを再帰的に判定
+
+    cout << ans << endl;
+} 
