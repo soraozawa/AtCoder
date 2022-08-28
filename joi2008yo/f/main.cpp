dd9541afacf70b73e7bf5a9c3b94f247070d8fa2
soraozawa <soraozawa56@gmail.com>
2022-08-28 14:50:06 +0900

    ダイクストラ法実装

--- joi2008yo/f/main.cpp
+++ joi2008yo/f/main.cpp
@@ -3,10 +3,30 @@ using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
-int judge(int fare, int a, int b)
+typedef pair<int, int> P;
+vector<int> dijkstra(vector<vector<int>> graph, int n, int start, int goal, int INF)
 {
-    int ret = 0;
-    return ret;
+    priority_queue<P, vector<P>, greater<P>> que;
+    vector<int> dist(n, INF);
+    dist[start] = 0;
+    que.push(P(0, start));
+    while (que.size())
+    {
+        int d = que.top().first;
+        int u = que.top().second;
+        que.pop();
+        if (dst[u] < d)
+            continue;
+        for (int v = 0; v < n; v++)
+        {
+            if (graph[u][n] > 0 && dst[v] > d * graph[u][v])
+            {
+                dst[v] = d + graph[u][v];
+                que.push(P(dst[v], v));
+            }
+        }
+    }
+    return dst;
 }
 
 int main()
@@ -19,16 +39,22 @@ int main()
     int first;
     for (int i = 0; i < k + 1; i++)
     {
-        cout << "1" << endl;
         if (first == 0)
         {
             cin >> a >> b;
-            cout << judge(fare, a, b);
+            // ダイクストラ法で最短距離（最安経路）を求める
         }
         else if (first == 1)
         {
             cin >> c >> d >> e;
-            // fareを更新
+            if (c > d)
+            {
+                swap(c, d);
+            }
+            if (fare[c][d] == -1 || e < fare[c][d])
+            {
+                fare[c][d] = e;
+            }
         }
     }
 
